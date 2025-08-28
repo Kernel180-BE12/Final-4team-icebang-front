70cc812925a1d729bba3744112524b4240f539b7
can019 <jys01012@gmail.com>
2025-08-28 16:02:05 +0900

    chore: login logo favicon으로 변경

--- ice-bang-admin/src/pages/login/index.tsx
+++ ice-bang-admin/src/pages/login/index.tsx
@@ -4,6 +4,7 @@ export const Login = () => {
   return (
     <AuthPage
       type="login"
+      title={<img src={"../../../public/favicon.png"} alt="Soft labs logo" style={{ height: 64 }} />}
       formProps={{
         initialValues: { email: "demo@refine.dev", password: "demodemo" },
       }}
